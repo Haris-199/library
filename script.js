4acac950984838812b9b9a92a54624d3a45b5fde
Haris <haris503@hotmail.com>
2024-07-07 05:53:09 -0400

    Create function to display books

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const books = document.getElementById("books");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,7 +11,44 @@ function Book(title, author, pages, read) {
     }
 }
 
+myLibrary.push(new Book("1984", "George Orwell", 449, "not read"));
+myLibrary.push(new Book("The Great Gatsby", "F. Scott Fitzgerald", 73, "read"));
+myLibrary.push(new Book("Pride and Prejudice", "Jane Austen", 340, "not read"));
+
 function addBookToLibrary() {
     const book = new Book();
     myLibrary.push(book);
 }
+
+function display() {
+    while (books.firstChild)
+        books.removeChild(books.firstChild);
+
+    myLibrary.forEach( book => {
+        const container = document.createElement("div");
+        const title = document.createElement("h3");
+        const author = document.createElement("h3");
+        const pages = document.createElement("p");
+        const read = document.createElement("p");
+
+        title.innerText = book.title;
+        author.innerText = `by ${book.author}`;
+        pages.innerText = `${book.pages} Pages`;
+        read.innerText = book.read;
+
+        container.appendChild(title);
+        container.appendChild(author);
+        container.appendChild(pages);
+        container.appendChild(read);
+        container.classList.add("book-card");
+        
+        if (book.read === "read")
+            container.classList.add("read");
+        else if (book.read === "not read")
+            container.classList.add("not-read");
+
+        books.appendChild(container);
+    });
+}
+
+display();
